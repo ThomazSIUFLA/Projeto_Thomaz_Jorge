fcb947dd8d5c88839dddbbceeaa982e7c87065c6
ThomazSIUFLA <thomaz.jorge@estudante.ufla.br>
2020-06-10 13:08:20 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,29 @@
+﻿public class C{
+	
+	private String C1;
+	private int C2;
+	
+	public int getC1(){
+		return this.C1;
+	}
+	
+	public float getC2(){
+		returm this.C2;
+	}
+	
+	public void setC1(String C1){
+		this.C1 = C1;
+	}
+	
+	public void setC2(int C2){
+		this.C2 = C2;
+	}
+	
+	public void MC1(){
+		System.out.println("Método MC1");
+	}
+	
+	public void MC2(){
+		System.out.println("Método MC2");
+	}
+}
