751e01cb6746c10e4f0812c5dbdeb928e07fe959
ThomazSIUFLA <thomaz.jorge@estudante.ufla.br>
2020-06-10 13:49:11 -0300

    Criação da Classe D

---
+++ D.java
@@ -0,0 +1,29 @@
+﻿public class D{
+	
+	private int D1;
+	private float D2;
+	
+	public int getD1(){
+		return this.D1;
+	}
+	
+	public float getD2(){
+		returm this.D2;
+	}
+	
+	public void setD1(int D1){
+		this.D1 = D1;
+	}
+	
+	public void setD2(float D2){
+		this.D2 = D2;
+	}
+	
+	public void MD1(){
+		System.out.println("Método MD1");
+	}
+	
+	public void MD2(){
+		System.out.println("Método MD2");
+	}
+}
